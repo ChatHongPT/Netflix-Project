c614cc48e1940ea13233dead6578edc2cf105dd2
suk924600 <suk924600@naver.com>
2024-11-26 14:48:10 +0900

    auth 인증 기능 구현

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
+import { useAuth } from '../composables/useAuth'
+
+const { isAuthenticated, logout } = useAuth()
 </script>
 
 <template>
@@ -7,11 +10,14 @@ import { RouterLink } from 'vue-router'
     <div class="container mx-auto flex justify-between items-center">
       <RouterLink to="/" class="text-xl font-bold">MoviePro</RouterLink>
       <div class="space-x-4">
-        <RouterLink to="/" class="hover:text-gray-300">홈</RouterLink>
-        <RouterLink to="/trending" class="hover:text-gray-300">대세 콘텐츠</RouterLink>
-        <RouterLink to="/browse" class="hover:text-gray-300">찾아보기</RouterLink>
-        <RouterLink to="/my-list" class="hover:text-gray-300">내가 찜한 콘텐츠</RouterLink>
-        <RouterLink to="/login" class="hover:text-gray-300">로그인</RouterLink>
+        <template v-if="isAuthenticated">
+          <RouterLink to="/" class="hover:text-gray-300">홈</RouterLink>
+          <RouterLink to="/trending" class="hover:text-gray-300">대세 콘텐츠</RouterLink>
+          <RouterLink to="/browse" class="hover:text-gray-300">찾아보기</RouterLink>
+          <RouterLink to="/my-list" class="hover:text-gray-300">내가 찜한 콘텐츠</RouterLink>
+          <button @click="logout" class="hover:text-gray-300">로그아웃</button>
+        </template>
+        <RouterLink v-else to="/signin" class="hover:text-gray-300">로그인</RouterLink>
       </div>
     </div>
   </nav>
