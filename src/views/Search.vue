338b086fdb23bb180a26f346caa561086f645e91
suk924600 <suk924600@naver.com>
2024-11-26 12:05:48 +0900

    setting

---
+++ src/views/Search.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="search">
+    <h1>찾아보기</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.search {
+  padding-top: 70px;
+}
+</style>
